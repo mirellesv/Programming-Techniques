f9a56a6831f2ce2c3eacf8a5b974d5c79a3b79df
Mirelle Silva <mirelle.sv.vieira@gmail.com>
2023-02-05 12:50:21 -0300

    Update Kruskal.cpp

--- Problems/DarkRoads/Kruskal.cpp
+++ Problems/DarkRoads/Kruskal.cpp
@@ -4,59 +4,71 @@ using namespace std;
 
 using iPair = pair<int, int>;
 
-struct Graph{
-    int V, E;
-    vector < pair<int, iPair> > edges;
-
-    Graph(int V, int E){
-        this->V = V;
-        this->E = E;
-    }
-
-    void addEdge(int u, int v, int w){
-        edges.push_back({w, {u, v}});
-    }
-
-    int kruskalMST();
+class Graph{
+    private:
+        int V;
+        int E;
+        vector < pair<int, iPair> > edges;
+    public:
+        Graph(int new_V, int new_E){ // Constructor
+            V = new_V;
+            E = new_E;
+        }
+        // Acessors and mutators
+        int getV() const{
+            return V;
+        }
+        void setV(int new_V){
+            V = new_V;
+        }
+        int getE() const{
+            return E;
+        }
+        void setE(int new_E){
+            E = new_E;
+        }
+        void setEdge(int u, int v, int w){
+            edges.push_back({w, {u, v}});
+        }
+        int kruskalMST();
 };
 
-struct DisjointSets{
-    int *parent, *rnk;
-    int n;
+class DisjointSets{
+    private:
+        int *parent, *rnk;
+        int n;
 
-    DisjointSets(int n){
-        this->n = n;
-        parent = new int[n + 1];
-        rnk = new int[n + 1];
+    public:
+        DisjointSets(int new_n){ // Constructor
+            int i;
+            n = new_n;
+            parent = new int[n + 1];
+            rnk = new int[n + 1];
 
-        for(int i = 0; i <= n; i++){
-            rnk[i] = 0;
-
-            parent[i] = i;
-        }
-    }
-
-    int find(int u){
-        if(u != parent[u]){
-            parent[u] = find(parent[u]);
-        }
-        return parent[u];
-    }
-
-    void merge(int x, int y){
-        x = find(x);
-        y = find(y);
-
-        if(rnk[x] > rnk[y]){
-            parent[y] = x;
-        } else{
-            parent[x] = y;
+            for(i = 0; i <= n; i++){
+                rnk[i] = 0;
+                parent[i] = i;
+            }
         }
 
-        if(rnk[x] == rnk[y]){
-            rnk[y]++;
+        int find(int u){
+            if(u != parent[u])
+                parent[u] = find(parent[u]);
+            return parent[u];
         }
-    }
+
+        void merge(int x, int y){
+            x = find(x);
+            y = find(y);
+
+            if(rnk[x] > rnk[y])
+                parent[y] = x;
+            else
+                parent[x] = y;
+
+            if(rnk[x] == rnk[y])
+                rnk[y]++;
+        }
 };
 
 int Graph::kruskalMST(){
@@ -91,20 +103,20 @@ int main(){
     int V = 9, E = 14;
     Graph g(V, E);
 
-    g.addEdge(0, 1, 4);
-    g.addEdge(0, 7, 8);
-    g.addEdge(1, 2, 8);
-    g.addEdge(1, 7, 11);
-    g.addEdge(2, 3, 7);
-    g.addEdge(2, 8, 2);
-    g.addEdge(2, 5, 4);
-    g.addEdge(3, 4, 9);
-    g.addEdge(3, 5, 14);
-    g.addEdge(4, 5, 10);
-    g.addEdge(5, 6, 2);
-    g.addEdge(6, 7, 1);
-    g.addEdge(6, 8, 6);
-    g.addEdge(7, 8, 7);
+    g.setEdge(0, 1, 4);
+    g.setEdge(0, 7, 8);
+    g.setEdge(1, 2, 8);
+    g.setEdge(1, 7, 11);
+    g.setEdge(2, 3, 7);
+    g.setEdge(2, 8, 2);
+    g.setEdge(2, 5, 4);
+    g.setEdge(3, 4, 9);
+    g.setEdge(3, 5, 14);
+    g.setEdge(4, 5, 10);
+    g.setEdge(5, 6, 2);
+    g.setEdge(6, 7, 1);
+    g.setEdge(6, 8, 6);
+    g.setEdge(7, 8, 7);
 
     cout << "Edges of MST are \n";
     int mst_wt = g.kruskalMST();
